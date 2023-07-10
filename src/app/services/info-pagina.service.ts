17fcd8cd0e557ac9a111b6cae6bfec6d20a21512
alexiaCat <alexia_12ceci@hotmail.com>
2023-07-10 04:28:12 -0400

    add info with firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,13 +9,26 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-page.json')
       .subscribe((resp: InfoPagina) => {
-
         this.cargada = true;
         this.info = resp;
+      });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://portafolio-bbe21-default-rtdb.asia-southeast1.firebasedatabase.app/.json')
+      .subscribe((res: any) => {
+        this.equipo = res.equipo;
+        console.log(this.equipo);
       });
   }
 }

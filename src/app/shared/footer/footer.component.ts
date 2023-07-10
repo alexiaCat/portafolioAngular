4844c3fc4c56465d3b7c40cca7ec6062520acc08
alexiaCat <alexia_12ceci@hotmail.com>
2023-07-10 03:17:59 -0400

    add animated

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { InfoPaginaService } from '../../services/info-pagina.service';
 
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',

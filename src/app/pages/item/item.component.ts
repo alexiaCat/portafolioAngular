f4333280f2b4d2763f74fedcf98b5af80deb421d
alexiaCat <alexia_12ceci@hotmail.com>
2023-07-11 06:11:00 -0400

    add

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { dataProducto } from '../../interfaces/info-prod.interface'
 
 @Component({
   selector: 'app-item',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class ItemComponent {
 
+
+  producto!: dataProducto;
+  id!: String;
+
+
+  constructor(private route: ActivatedRoute, public productoService: ProductosService) {
+    this.route.params.subscribe(parametros => {
+      this.productoService.getProducto(parametros['id'])
+        .subscribe(( producto: dataProducto) =>{
+          this.producto = producto;
+          this.id = parametros['id'];
+        });
+    });
+  }
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,18 +9,60 @@ export class ProductosService {
 
   cargando = true;
   productos: infoProducto[] = [];
+  productosFiltrado: infoProducto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
-  private cargarProductos() {
-    this.http.get('https://portafolio-bbe21-default-rtdb.asia-southeast1.firebasedatabase.app/productos_idx.json')
-      .subscribe((res: any) => {
-        this.productos = res;
-
-        this.cargando = false;
-
-      });
+  private cargarProductos(): Promise<void> {
+    return new Promise<void>((res, rej) => {
+      this.http.get('https://portafolio-bbe21-default-rtdb.asia-southeast1.firebasedatabase.app/productos_idx.json')
+        .subscribe((response: any) => {
+          this.productos = response;
+          this.cargando = false;
+          res();
+        }, error => {
+          rej(error);
+        });
+    });
   }
+
+
+
+  getProducto(id: string) {
+    return this.http.get(`https://portafolio-bbe21-default-rtdb.asia-southeast1.firebasedatabase.app/productos/${id}.json`)
+  }
+
+
+  buscarProducto(termino: string) {
+
+    if (this.productos.length === 0) {
+      this.cargarProductos().then(() => {
+        this.filtarProductos(termino);
+      });
+
+    } else {
+      this.filtarProductos(termino);
+
+    }
+  }
+
+
+  private filtarProductos(termino: string) {
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach(prod => {
+      if (prod.titulo) {
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+        if (prod.categoria && prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+          this.productosFiltrado.push(prod);
+        }
+      }
+    });
+  }
+
+
+
 }
